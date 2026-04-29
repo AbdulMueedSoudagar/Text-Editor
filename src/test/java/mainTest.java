9c5307bfffb88aeeb9fe3ee8ccced913f3b5628c
Abdul Mueed Soudagar <soudagarmueed162002@gmail.com>
2026-04-29 14:53:56 +0530

    made changes

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,8 +1,8 @@
 package test.java;
 
 import main.java.main;
-// import org.junit.Test;
-// import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class mainTest {
 
